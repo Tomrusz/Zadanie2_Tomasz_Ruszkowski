c41b1ad0e09ac6cfd9181b37419ffb73bacfd5b4
Tomrusz <Tomruszbiznes@gmail.com>
2023-06-29 19:02:31 +0200

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie2_Tomasz_Ruszkowski
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Kalkulator Kalkulator { get; } = new Kalkulator();
+        public MainWindow()
+        {
+            DataContext = Kalkulator;
+            InitializeComponent();
+        }
+
+        private void Cyfra(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.WprowadźCyfrę(((Button)sender).Content.ToString()
+);
+        }
+
+        private void Przecinek(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.WprowadźPrzecinek();
+        }
+
+        private void ZmianaZnaku(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.ZmieńZnak();
+        }
+
+        private void KasowanieZnaku(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.KasujZnak();
+        }
+
+        private void Czyszczenie(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.CzyśćWszystko();
+        }
+
+        private void CzyszczenieWprowadzenia(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.CzyśćWynik();
+        }
+
+        private void DziałanieDwuargumentowe(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.WprowadźDziałanieDwuargumentowe(((Button)sender).Content.ToString());
+        }
+
+        private void DziałanieJednoargumentowe(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.WykonajDziałanieJednoargumentowe(((Button)sender).Content.ToString());
+        }
+
+        private void RównaSię(object sender, RoutedEventArgs e)
+        {
+            Kalkulator.WykonajDziałanie();
+        }
+    }
+}
---
+++ kalkulator.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Zadanie2_Tomasz_Ruszkowski
+{
+    class Kalkulator : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string? wynik = "0";
+        private string? operacja = null;
+        private double? operandLewy = null;
+        private double? operandPrawy = null;
+        private bool flaga = false;
+
+        public string Wynik
+        {
+            get => wynik;
+            set
+            {
+                wynik = value;
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs("Wynik")
+                    );
+            }
+        }
+
+        public string Działanie
+        {
+            get
+            {
+                if (operandLewy == null)
+                    return "";
+                else if (operandPrawy == null)
+                    return $"{operandLewy} {operacja}";
+                else
+                    return $"{operandLewy} {operacja} {operandPrawy} = ";
+            }
+        }
+
+        internal void WprowadźCyfrę(string? cyfra)
+        {
+            if (flaga)
+            {
+                Wynik = "0";
+                flaga = false;
+            }
+
+            if (Wynik == "0")
+                Wynik = cyfra;
+            else
+                Wynik += cyfra;
+        }
+
+        internal void WprowadźPrzecinek()
+        {
+            if (flaga)
+                Wynik = "0";
+            if (Wynik.Contains(','))
+                return;
+            else
+                Wynik += ",";
+        }
+
+        internal void ZmieńZnak()
+        {
+            if (flaga)
+                Wynik = "0";
+            if (Wynik == "0")
+                return;
+            else if (Wynik[0] == '-')
+                Wynik = Wynik.Substring(1);
+            else
+                Wynik = "-" + Wynik;
+        }
+
+        internal void KasujZnak()
+        {
+            if (flaga)
+                Wynik = "0";
+            if (Wynik == "0")
+                return;
+            else if (Wynik == "-0," || Wynik.Length == 1 || (Wynik.Length == 2 && Wynik[0] == '-'))
+                Wynik = "0";
+            else
+                Wynik = Wynik.Substring(0, Wynik.Length - 1);
+        }
+
+        internal void CzyśćWszystko()
+        {
+            CzyśćWynik();
+            operacja = null;
+            operandLewy = operandPrawy = null;
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs("Działanie")
+                );
+        }
+
+        internal void CzyśćWynik()
+        {
+            Wynik = "0";
+        }
+
+        internal void WprowadźOperacja(string operacja)
+        {
+            if (this.operacja != null)
+            {
+                WykonajDziałanie();
+                this.operacja = operacja;
+            }
+            else
+            {
+                operandLewy = Convert.ToDouble(wynik);
+                this.operacja = operacja;
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs("Działanie")
+                    );
+            }
+
+            wynik = "0";
+        }
+
+        internal void WprowadźDziałanieDwuargumentowe(string działanie)
+        {
+            if (operandLewy == null)
+            {
+                operandLewy = Convert.ToDouble(Wynik);
+                operacja = działanie;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Działanie"));
+                wynik = "0";
+            }
+            else if (operacja == null)
+            {
+                operacja = działanie;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Działanie"));
+                wynik = "0";
+            }
+            else
+            {
+                operandPrawy = Convert.ToDouble(Wynik);
+                WykonajDziałanie();
+                operacja = działanie;
+                operandLewy = Convert.ToDouble(Wynik);
+                operandPrawy = null;
+                wynik = "0";
+                flaga = true;
+            }
+        }
+
+        internal void WykonajDziałanie()
+        {
+            if (operandPrawy == null)
+                operandPrawy = Convert.ToDouble(Wynik);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Działanie"));
+
+            switch (operacja)
+            {
+                case "+":
+                    Wynik = $"{operandLewy + operandPrawy}";
+                    break;
+                case "-":
+                    Wynik = $"{operandLewy - operandPrawy}";
+                    break;
+                case "×":
+                    Wynik = $"{operandLewy * operandPrawy}";
+                    break;
+                case "÷":
+                    Wynik = $"{operandLewy / operandPrawy}";
+                    break;
+                case "x^y":
+                    Wynik = $"{Math.Pow((double)operandLewy, operandPrawy.Value)}";
+                    break;
+                case "%":
+                    Wynik = $"{(operandPrawy / 100) * operandLewy}";
+                    break;
+                case "mod":
+                    Wynik = $"{operandLewy % operandPrawy}";
+                    break;
+            }
+        }
+
+        internal void WykonajDziałanieJednoargumentowe(string działanie)
+        {
+            if (operandLewy == null)
+                operandLewy = Convert.ToDouble(Wynik);
+
+            switch (działanie)
+            {
+                case "√":
+                    operandLewy = Math.Sqrt(operandLewy.Value);
+                    break;
+                case "1/x":
+                    operandLewy = 1 / operandLewy;
+                    break;
+                case "x!":
+                    operandLewy = ObliczSilnię(Convert.ToInt32(operandLewy));
+                    break;
+                case "log":
+                    operandLewy = Math.Log10(operandLewy.Value);
+                    break;
+                case "ln":
+                    operandLewy = Math.Log(operandLewy.Value);
+                    break;
+                case "round down":
+                    operandLewy = Math.Floor(operandLewy.Value);
+                    break;
+                case "round up":
+                    operandLewy = Math.Ceiling(operandLewy.Value);
+                    break;
+            }
+
+            Wynik = $"{operandLewy}";
+            flaga = true;
+            operacja = null;
+            operandPrawy = null;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Działanie"));
+        }
+
+        private double ObliczSilnię(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * ObliczSilnię(n - 1);
+        }
+    }
+}
